310283cc8a9ab31b2c02d3582736fc4e11248e26
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-01-31 16:15:12 +0100

    Updates

---
+++ precache-manifest.6c2e8184fd39e9e1287c0bd9369d8ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8c7bb6facf32fe23f6d7ce906c6ee8",
+    "url": "/gr12/index.html"
+  },
+  {
+    "revision": "ee615dd71cffd63b8780",
+    "url": "/gr12/static/css/main.6bc8a8dc.chunk.css"
+  },
+  {
+    "revision": "91e1afedf5f08785d3d3",
+    "url": "/gr12/static/js/2.4953688a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/gr12/static/js/2.4953688a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee615dd71cffd63b8780",
+    "url": "/gr12/static/js/main.00497394.chunk.js"
+  },
+  {
+    "revision": "bb7a5fcf9137ae6d4310",
+    "url": "/gr12/static/js/runtime-main.f70bbf11.js"
+  }
+]);
